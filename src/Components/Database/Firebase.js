842123fe60568f14e6b200e8be29a2e091ef44cd
vijay378 <58065296+vijay378@users.noreply.github.com>
2021-09-07 00:29:00 +0530

    Update Firebase.js

--- src/Components/Database/Firebase.js
+++ src/Components/Database/Firebase.js
@@ -1,13 +1,21 @@
 import firebase from 'firebase'
 const firebaseConfig = {
-  apiKey: "AIzaSyD1pJ91yqEBttKQltRllmgsfmyRKamT3Ds",
-    authDomain: "project01-9c31a.firebaseapp.com",
-    databaseURL: "https://project01-9c31a-default-rtdb.firebaseio.com",
-    projectId: "project01-9c31a",
-    storageBucket: "project01-9c31a.appspot.com",
-    messagingSenderId: "632973090927",
-    appId: "1:632973090927:web:c8d3696be7a4756d9d6d9a",
-    measurementId: "G-5BMETJY2NJ"
+   apiKey: "AIzaSyD1pJ91yqEBttKQltRllmgsfmyRKamT3Ds",
+
+  authDomain: "project01-9c31a.firebaseapp.com",
+
+  databaseURL: "https://project01-9c31a-default-rtdb.firebaseio.com",
+
+  projectId: "project01-9c31a",
+
+  storageBucket: "project01-9c31a.appspot.com",
+
+  messagingSenderId: "632973090927",
+
+  appId: "1:632973090927:web:962f8a2f804125f69d6d9a",
+
+  measurementId: "G-KRRE6H72NN"
+
 };
 
 firebase.initializeApp(firebaseConfig);
